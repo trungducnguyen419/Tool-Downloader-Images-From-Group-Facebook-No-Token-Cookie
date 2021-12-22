517dd73200d17faf5db7f5f30345b6c58cc49c44
Nguyễn Trung Đức <54103645+trungducmcgamer@users.noreply.github.com>
2021-12-22 18:18:20 +0000

    Add files via upload

---
+++ Tool Download All Images From Group/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿
+namespace Tool_Download_All_Images_From_Group
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 225);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(87, 25);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Download";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 14);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "List Id Group:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 26);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(249, 193);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(272, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(175, 241);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Lưu ý: Tool sẽ tải chậm để tránh bọn facebook block request limit,nếu muốn tải nh" +
+    "anh không bị delay thì áp dụng proxy vô code là tải nhanh. Tỉ lệ download tất cả" +
+    " ảnh từ group là 90%";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(459, 257);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Tool Download All Images From Group";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Tool Download All Images From Group/Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using System.Net;
+
+namespace Tool_Download_All_Images_From_Group
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            CreateFolder(Application.StartupPath + "\\Images");
+
+            TextBox.CheckForIllegalCrossThreadCalls = false;
+            Button.CheckForIllegalCrossThreadCalls = false;
+        }
+        bool isStop = false;
+        Random rand = new Random();
+        public static bool IsValidJson(string src)
+        {
+            try
+            {
+                var asToken = JToken.Parse(src);
+                return asToken.Type == JTokenType.Object || asToken.Type == JTokenType.Array;
+            }
+            catch (Exception) 
+            {
+                return false;
+            }
+        }
+        public static bool CreateFolder(string path)
+        {
+            try
+            {
+                if (!Directory.Exists(path)) //nếu không có folder images
+                {
+                    Directory.CreateDirectory(path); //tạo folder images
+                }
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            if (button1.Text == "Download")
+            {
+                button1.Text = "Stop";
+                isStop = false; //bắt đầu download
+                string[] idgroups = textBox1.Lines; //lấy danh sách id group từ textbox
+                await Task.Run(async () => //tạo luồng
+                {
+                    foreach (var idgroup in idgroups) //lấy từng id group một
+                    {
+                        if (isStop) return; //isStop bằng true thì sẽ dừng lại
+                        if (!string.IsNullOrEmpty(idgroup)) //nếu id group không bị rỗng
+                        {
+                            HttpClient httpClient = new HttpClient();
+                            httpClient.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36"); //thêm user agent để lấy đc danh sách ảnh
+                            httpClient.DefaultRequestHeaders.Add("Cookie", "locale=vi_VN"); //đặt ngôn ngữ facebook thành ngôn ngữ Việt Nam
+                            httpClient.DefaultRequestHeaders.Add("sec-fetch-site", "same-origin"); //thêm sec-fetch-site để lấy đc danh sách ảnh
+                            var content = new FormUrlEncodedContent(new[]
+                            {
+                            new KeyValuePair<string, string>("doc_id", "4430099110431117"),
+                            new KeyValuePair<string, string>("variables", "{\"groupID\":\"" + idgroup + "\",\"scale\":1,\"useCometPhotoViewerPlaceholderFrag\":false,\"count\":1000}")
+                        });
+                            var response = await httpClient.PostAsync("https://www.facebook.com/api/graphql/", content);
+                            string result = response.Content.ReadAsStringAsync().Result;
+                            if (!IsValidJson(result)) goto DONE_ID_GROUP;
+                            CreateFolder(Application.StartupPath + "\\Images\\" + idgroup);
+                            dynamic json = JsonConvert.DeserializeObject(result); //parse json
+                            foreach (dynamic jsonCon in json["data"]["group"]["group_mediaset"]["media"]["edges"])
+                            {
+                                try
+                                {
+                                    new WebClient().DownloadFile((string)jsonCon["node"]["image"]["uri"], (string)Application.StartupPath + "\\Images\\" + idgroup + "\\" + jsonCon["node"]["id"] + ".jpg");
+                                }
+                                catch (Exception) { }
+                                if (isStop) return; //isStop bằng true thì sẽ dừng lại
+                            }
+                            var end_cursor = json["data"]["group"]["group_mediaset"]["media"]["page_info"]["end_cursor"];
+                        BACK_DOWNLOAD:;
+                            if (end_cursor != null)
+                            {
+                                Thread.Sleep(TimeSpan.FromMinutes(rand.Next(3, 6)));
+                                content = new FormUrlEncodedContent(new[]
+                                {
+                                new KeyValuePair<string, string>("doc_id", "4544387022318594"),
+                                new KeyValuePair<string, string>("variables", "{\"count\":1000,\"cursor\":\"" + (string)end_cursor + "\",\"scale\":1,\"useCometPhotoViewerPlaceholderFrag\":false,\"id\":\"" + idgroup + "\"}")
+                            });
+                                response = await httpClient.PostAsync("https://www.facebook.com/api/graphql/", content);
+                                result = response.Content.ReadAsStringAsync().Result;
+                                json = JsonConvert.DeserializeObject(result); //parse json
+                                foreach (dynamic jsonCon in json["data"]["node"]["group_mediaset"]["media"]["edges"])
+                                {
+                                    try
+                                    {
+                                        new WebClient().DownloadFile((string)jsonCon["node"]["image"]["uri"], (string)Application.StartupPath + "\\Images\\" + idgroup + "\\" + jsonCon["node"]["id"] + ".jpg");
+                                    }
+                                    catch (Exception) { }
+                                    if (isStop) return; //isStop bằng true thì sẽ dừng lại
+                                }
+                                end_cursor = json["data"]["node"]["group_mediaset"]["media"]["page_info"]["end_cursor"]; //lấy cursor để lấy danh sách tiếp theo
+                                goto BACK_DOWNLOAD; //quay lại lấy tiếp danh sách
+                            }
+                        }
+                    DONE_ID_GROUP:;
+                    }
+                    button1.Text = "Download";
+                });
+            }   
+            else
+            {
+                button1.Text = "Download";
+                isStop = true; //dừng download
+            }    
+        }
+    }
+}
